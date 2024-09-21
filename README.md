11075d5a59076f07f57ac3878af1497ceee14780
Roman <malinovroman@mail.ru>
2024-09-21 19:14:09 +0300

    chore: обновление README с ссылкой на тестовое задание

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
+# Next.js Project
+
+[Check out the test assignment](https://csoft-test-tasks-c9c8.vercel.app/)
+
 This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
+
+[Посмотреть тестовое задание](https://csoft-test-tasks-c9c8.vercel.app/)
 
 ## Getting Started
 
--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,9 @@
-// /** @type {import('next').NextConfig} */
-// const isProd = process.env.NODE_ENV === 'production';
-
-// const nextConfig = {
-//   output: 'export',
-//   basePath: isProd ? '/csoft-test-tasks' : '',
-//   assetPrefix: isProd ? '/csoft-test-tasks/' : '',
-// };
-
-// export default nextConfig;
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  output: 'export',
+};
 
 export default nextConfig;
+
